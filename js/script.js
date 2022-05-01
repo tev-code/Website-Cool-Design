89371292179a9bb68de4aa27203fdb8e57dbb80b
Tevin <tevin@trade245.com>
2022-05-01 12:56:19 +0200

    latest

--- js/script.js
+++ js/script.js
@@ -12,3 +12,14 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
   
 }
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home, .about, .winter, .video, .contact, .blogs`, {
+    interval: 100
+})
